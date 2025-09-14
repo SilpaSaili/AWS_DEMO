0ea18744db3ddcbf2aa0469d17d5237c78fdf04e
SilpaSaili <silpa.saili.b@gmail.com>
2025-09-14 18:17:43 -0500

    Update AwsLearningApplicationTests.java

--- src/test/java/com/demo/aws/AwsLearningApplicationTests.java
+++ src/test/java/com/demo/aws/AwsLearningApplicationTests.java
@@ -3,10 +3,10 @@ package com.demo.aws;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AwsLearningApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
